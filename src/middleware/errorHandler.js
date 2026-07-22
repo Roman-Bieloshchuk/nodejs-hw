75c2c39d3c096e251f02d5004498bc8787e553c4
Roman Bieloshchuk <beloshchuk@gmail.com>
2026-07-22 19:48:18 +0300

    Fix update options and error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,8 +1,16 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
   void req;
   void next;
 
-  res.status(err.status || 500).json({
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  res.status(500).json({
     message: err.message,
   });
 };
